599a7d7493efc4c76b1c54ed716cc7868810408a
Lukman Abiola <herbay4luv@gmail.com>
2023-04-03 17:12:31 +0100

    commit

---
+++ conditional.js
@@ -0,0 +1,15 @@
+// var nameOfstudent ="paul"
+// var number = 5;
+
+// // if (nameOfstudent == "paul") {
+// //     alert ("This is john")
+// // } else {
+// //     alert("tani e?")
+// // }
+// if (number == 5){
+//     alert("i am 5")
+// }
+// else {
+//     alert("i dont know you")
+// }
+
---
+++ intro.js
@@ -0,0 +1,55 @@
+// var divShow = document.getElementById("showUp").innerHTML;
+// console.log(divShow);
+
+// var calc = 2+2;
+// console.log(calc);
+// divShow.innerHTML= calc;
+// divShow.style.color="red"
+
+
+// function showGrade () {
+//     var inputShow = document.getElementById
+//     ("input");
+//     inputShow.value =78;
+    
+// }
+// var myAge =5;
+// if(myAge<7){
+//     alert("coke")
+// }
+// else if(myAge<8){
+//     alert("fanta")
+// }
+
+var myNumber=prompt("Enter a number")
+if(myNumber%3==0 && myNumber %5==0){
+    alert("fizz buzz")}
+
+// else if(myNumber%3==0){
+//     alert("fizz")
+// }
+// else if(myNumber%5==0){
+//     alert("buzz")
+// }
+// wed class
+
+// function checkScore () {
+//      var songExcellent = new Audio("audio/party.mp3")
+//      var songGood =new Audio("audio/story.mp3")
+//     var scoreOfUser = document.getElementById
+//     ('userScore');
+//     var userScored = scoreOfUser.value;
+//     if(userScored >=70 && userScored <=100){
+//     // console.log("Excellent");
+//     showScore.innerHTML = "Excellent"
+//     showScore.style.color = "green";
+//     songExcellent.play();
+    
+// } 
+//     else if(userScored >=60 && userScored <70){
+//     // console.log("very good")
+//     showScore.innerHTML = "very good"
+//     showScore.style.color = "brown"
+//     songGood.play();
+// }
+//     }
